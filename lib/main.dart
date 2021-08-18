17f135e8be506fc88419f24e8a9f3b167b5a2ca6
dhineshkathirvel <Dhinesh1607>
2021-08-19 04:20:22 +0530

    Working with list of data in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_list_data/quote.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ListData(),
+  ));
+}
+
+class ListData extends StatefulWidget {
+  @override
+  _ListDataState createState() => _ListDataState();
+}
+
+class _ListDataState extends State<ListData> {
+
+  List<Quote> quotes=[
+    Quote(text: "Design is not just what it looks like and feels like. Design is how it works.", author: "Steve Jobs"),
+    Quote(text: "Innovation distinguishes between a leader and a follower.", author: "Steve Jobs"),
+    Quote(text: "I want to put a dent in the universe.", author: "Steve Jobs")
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[500],
+      appBar: AppBar(
+        title: Text("Steve Jobs Quotes"),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      )
+    );
+  }
+}
